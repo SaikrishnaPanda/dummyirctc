da960c952dba9eda9adcda5a7399c03b7c088728
Saikrishna <s.krishna.panda@gail.com>
2025-04-12 21:43:58 +0530

    trainschedule module startup

---
+++ trainschedule/src/main/java/com/dummyirctc/trainschedule/TrainscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.dummyirctc.trainschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainscheduleApplication.class, args);
+	}
+
+}
---
+++ trainschedule/src/test/java/com/dummyirctc/trainschedule/TrainscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummyirctc.trainschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
